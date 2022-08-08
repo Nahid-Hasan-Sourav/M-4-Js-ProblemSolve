158db69ee407524c2cb6e1634829f099825e1646
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-08-09 02:16:56 +0600

    solve first problem

--- prb-1.js
+++ prb-1.js
@@ -2,3 +2,10 @@
 // apples. Write a program to help Harry calculate how much money the
 // shopkeeper will return. The total cost of 1 kg of oranges and 1 kg of
 // apples is tk 700.
+
+var HarryMomGiveTk=1000;
+var totalCoast=700;
+
+var Shopkeeper_Will_Return=(HarryMomGiveTk- totalCoast);
+
+console.log("Shopkeeper Will Return",Shopkeeper_Will_Return+" TK");
